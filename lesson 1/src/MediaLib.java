b43463397fe8d7e0d702c8790df681a42ce6c8d7
tavius84 <goldbrass8@gmail.com>
2020-02-12 10:05:51 -0600

    3

--- lesson 1/src/MediaLib.java
+++ lesson 1/src/MediaLib.java
@@ -3,12 +3,20 @@ public class MediaLib {
     public static void main(String[] args) {
         System.out.println("Welcome to your Media Library");
         Song song1 = new Song();
+        Books book1 = new Books();
+        Movies movie1 = new Movies();
         System.out.println(song1);
         song1.setTitle("Johnny B. Goode");
         System.out.println(song1.getTitle());
+        System.out.println(book1);
+        book1.setTitle("Children of the Corn");
+        System.out.println(book1.getTitle());
+        System.out.println(movie1);
+        movie1.setTitle("Battle LA");
+        System.out.println(movie1.getTitle());
 
 
-        
+
 
     }
 }
